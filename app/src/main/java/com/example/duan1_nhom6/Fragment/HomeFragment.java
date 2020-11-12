5d9f17c87bd2caadccec571d2f1810ff4afa259a
huynguyen199 <67964823+huynguyen199@users.noreply.github.com>
2020-11-12 16:18:16 +0700

    up 4h18

--- app/src/main/java/com/example/duan1_nhom6/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/duan1_nhom6/Fragment/HomeFragment.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.duan1_nhom6.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
 
@@ -30,6 +25,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_phone_detail, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
